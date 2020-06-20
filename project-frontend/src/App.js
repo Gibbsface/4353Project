419a31fb03bc2e90fd7b8dcd47ebf443fc571597
Gibbsface <cole.gibbs@gmail.com>
2020-06-20 14:23:22 -0500

    removed more comments

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import WelcomePage from "./pages/WelcomePage";
 
