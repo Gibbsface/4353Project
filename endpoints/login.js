392045692ca3995f6aa7b648e65f4f94a7f8ecda
Adair <adairf625@gmail.com>
2020-06-26 19:44:07 -0500

    login + register dummy api

--- endpoints/login.js
+++ endpoints/login.js
@@ -1,5 +1,39 @@
-const endpoint = function(request, response){
+const jwt = require('jsonwebtoken');
+const passwordHash = require('password-hash');
 
+const hardcode_creds = {
+    username: "test",
+    hashPass: passwordHash.generate("test", {
+        algorithm: "sha256",
+        iterations: 5,
+        saltLength: 32
+    }),
+    key: "secret"
+}
+
+const endpoint = function (request, response) {
+    let { username, password } = request.body;
+    console.log(username, password)
+    if (request.body.username == hardcode_creds.username && passwordHash.verify(password, hardcode_creds.hashPass)) {
+        let token = jwt.sign({data: username}, "secret", { expiresIn: 60 * 60 * 24 })
+        response.status(200);
+        response.cookie('token', token, {
+            maxAge: 1000*60*60*24
+        })
+        response.send(JSON.stringify({
+            success: true,
+            data: {
+                token  // generate token that expires in 1 day
+            }
+        }))
+    } else {
+        response.status(400);
+        response.send(JSON.stringify({
+            success: false,
+            data: {
+            }
+        }))
+    }
 }
 
 module.exports = endpoint;
--- endpoints/register.js
+++ endpoints/register.js
@@ -1,5 +1,22 @@
-const endpoint = function(request, response){
+let passwordHash = require('password-hash');
 
+const endpoint = function(request, response){
+    let {username, password, extrapass} = request.body;
+    if(password!==extrapass){
+        return response.send(JSON.stringify({
+            success: false,
+            data: "Password re-entry incorrect"
+        }))
+    }else{
+        let hashedPass = passwordHash.generate(password,{algorithm:'sha256',iterations:5,saltLength:32});
+        //
+        // insert user object into db here
+        //
+        return response.send(JSON.stringify({
+            success: true,
+            data: {}
+        }))
+    }
 }
 
 module.exports = endpoint;
