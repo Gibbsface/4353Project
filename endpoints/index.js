6c1a98e8eefd7f58f8bd6b01811b4eaa90ed5d17
Tristan <tristankylerjones@gmail.com>
2020-06-28 17:34:29 -0500

    Changing history to quote_history

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,8 +1,8 @@
 const fuel_quote = require('./fuel_quote');
-const history = require('./history');
+const quote_history = require('./quote_history');
 const login = require('./login');
 const register = require('./register');
 const profile_info = require('./profile_info');
 const profile_update = require('./profile_update');
 
-module.exports = {fuel_quote, history, login, register, profile_info, profile_update};
+module.exports = {fuel_quote, quote_history, login, register, profile_info, profile_update};
