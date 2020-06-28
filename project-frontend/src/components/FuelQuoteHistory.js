f7f340119d907d1fae34b9847fb0ada965251742
Tristan <tristankylerjones@gmail.com>
2020-06-28 17:33:18 -0500

    Updated FuelQuoteHistory frontend to fetch and display backend

--- project-frontend/src/components/FuelQuoteHistory.js
+++ project-frontend/src/components/FuelQuoteHistory.js
@@ -2,66 +2,60 @@ import React from "react";
 import "react-bulma-components/dist/react-bulma-components.min.css";
 
 export class History extends React.Component {
-
-  constructor(props){
-    super(props)
-    this.state = {entries: []}
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [],
+    };
   }
 
-  componentDidMount(){
-    fetch("http://localhost:8080/api/history",{
-      method:'get',
-      credentials:'include',
-      mode:'cors'
-    }).then(res=>{
-      res.json().then(result=>{
-        if(result.success){
-          this.setState({entries:result.data})
-        }
-      })
+  componentDidMount() {
+    fetch("http://localhost:8080/api/quote_history", {
+      method: "get",
+      credentials: "include",
+      mode: "cors",
     })
+      .then((res) => res.json())
+      .then((history) =>
+        this.setState({ history }, () =>
+          console.log("History fetched...", history)
+        )
+      );
   }
 
   render() {
-    let history;
-    if(this.state.entries.length==0){
-      history = null
-    }else{
-      history = this.state.entries.map(entry=>{
-        return (<tbody>
-          <th>{entry.gallons}</th>
-          <th>{entry.address}</th>
-          <th>{entry.date}</th>
-          <th>{entry.suggestedPrice}</th>
-          <th>{entry.totalPrice}</th>
-        </tbody>)
-      })
-    }
     return (
       <div className="container">
-          <div
-            class="is-overlay has-text-centered single-spaced"
-            style={{ top: 150 }}
-          >
-            <div class="container">
-              <div class="box">
-                <table class="table is-fullwidth">
-                  <thead>
-                    <tr>
-                      <th>Gallons Requested</th>
-                      <th>Delivery Address</th>
-                      <th>Delivery Date</th>
-                      <th>Suggested Price</th>
-                      <th>Total Price</th>
-                    </tr>
-                  </thead>
-                  {history}
-                </table>
-                {history==null?<p>No entries</p>:""}
-              </div>
+        <div
+          class="is-overlay has-text-centered single-spaced"
+          style={{ top: 150 }}
+        >
+          <div class="container">
+            <div class="box">
+              <table class="table is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>Gallons Requested</th>
+                    <th>Delivery Address</th>
+                    <th>Delivery Date</th>
+                    <th>Suggested Price</th>
+                    <th>Total Price</th>
+                  </tr>
+                </thead>
+                {this.state.history.map((history) => (
+                  <tr key={history.id}>
+                    <th>{history.gallonsRequested}</th>
+                    <th>{history.deliveryAddress}</th>
+                    <th>{history.deliveryDate} </th>
+                    <th>{history.suggestedPrice}</th>
+                    <th>{history.totalPrice}</th>
+                  </tr>
+                ))}
+              </table>
             </div>
           </div>
         </div>
+      </div>
     );
   }
 }
