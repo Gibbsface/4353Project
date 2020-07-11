d5d7db98cacd5071256d7f96f75aa0868d599863
Tristan Jones <tristankylerjones@gmail.com>
2020-07-11 04:42:44 -0500

    Working quote history

--- endpoints/quote_history.js
+++ endpoints/quote_history.js
@@ -1,26 +1,21 @@
+let { argv } = require("yargs");
+let { dbUsername, dbPassword } = argv;
+let connection = require("../database");
+
 const endpoint = function (request, response) {
-  const history = [
-    {
-      username: "test",
-      id: 1,
-      gallonsRequested: "5",
-      deliveryAddress: "0005 Example Dr",
-      deliveryDate: "6/28/2020",
-      suggestedPrice: "1",
-      totalPrice: "5",
-    },
-    {
-      username: "test",
-      id: 2,
-      gallonsRequested: "10",
-      deliveryAddress: "0010 Example St",
-      deliveryDate: "6/30/2020",
-      suggestedPrice: "2",
-      totalPrice: "20",
-    },
-  ];
-  response.contentType("application/json");
-  response.send(history);
+  console.log(request.username);
+  connection.query(
+    `SELECT * FROM fuel_quote WHERE id = '${request.username}'`,
+    function (error, rows, fields) {
+      if (!!error) {
+        console.log("Error in Query");
+      } else {
+        console.log("Successful Query");
+        response.contentType("application/json");
+        response.json(rows);
+      }
+    }
+  );
 };
 
 module.exports = endpoint;
