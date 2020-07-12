ceca9cdf71b7ae7f4c23120c39f077b966a19170
Adair <adairf625@gmail.com>
2020-07-12 18:48:18 -0500

    Refactor my stuff to use database file

--- endpoints/login.js
+++ endpoints/login.js
@@ -1,70 +1,39 @@
 const jwt = require('jsonwebtoken');
 const passwordHash = require('password-hash');
-let {argv} = require('yargs');
-let {dbUsername, dbPassword} = argv;
-const mysql = require('mysql');
-const hardcode_creds = {
-    username: "test",
-    hashPass: passwordHash.generate("test", {
-        algorithm: "sha256",
-        iterations: 5,
-        saltLength: 32
-    }),
-    key: "secret"
-}
+let connection = require('../database');
 
 const endpoint = function (request, response) {
     let { username, password } = request.body;
-    let connection = mysql.createConnection({
-        host: "localhost",
-        user: dbUsername,
-        password: dbPassword,
-        database: "sql_database",
-    });
-    connection.connect(e => {
-        if (e) {
-            return response.send(e);
+    connection.query(`SELECT id AS username, password AS hashedPass FROM user_credentials WHERE id="${username}"`, (err, res, fields) => {
+        if (err) {
+            return response.send(err);
         }
-        connection.query(`SELECT id AS username, password AS hashedPass FROM user_credentials WHERE id="${username}"`, (err, res, fields)=>{
-            if(err){
-                return response.send(err);
-            }
-            if(res.length==0)
-                return response.send(JSON.stringify({
-                    success:false,
-                    data:"No such user found"
-                }))
-            let check = passwordHash.verify(password, res[0].hashedPass);
-            if(check){
-                let token = jwt.sign({data: {username}}, "secret", { expiresIn: 60 * 60 * 24 })
-                response.cookie('token', token, {
-                    maxAge: 1000*60*60*24
-                })
-                response.contentType("application/json");
-                return response.send(JSON.stringify({
-                    success: true,
-                    data: {
-                        token
-                    }
-                }))
-            }else{
-                response.contentType("application/json");
-                return response.send(JSON.stringify({
-                    success:false,
-                    data:"Wrong password"
-                }))
-            }
-        })
+        if (res.length == 0)
+            return response.send(JSON.stringify({
+                success: false,
+                data: "No such user found"
+            }))
+        let check = passwordHash.verify(password, res[0].hashedPass);
+        if (check) {
+            let token = jwt.sign({ data: { username } }, "secret", { expiresIn: 60 * 60 * 24 })
+            response.cookie('token', token, {
+                maxAge: 1000 * 60 * 60 * 24
+            })
+            response.contentType("application/json");
+            return response.send(JSON.stringify({
+                success: true,
+                data: {
+                    token
+                }
+            }))
+        } else {
+            response.contentType("application/json");
+            return response.send(JSON.stringify({
+                success: false,
+                data: "Wrong password"
+            }))
+        }
     });
-    // });
-    // if (request.body.username == hardcode_creds.username && passwordHash.verify(password, hardcode_creds.hashPass)) {
-  
-    // } else {
-    //     response.send(JSON.stringify({
-    //         success: false,
-    //         data: null
-    //     }))
-    // }
 }
 
 module.exports = endpoint;
--- endpoints/register.js
+++ endpoints/register.js
@@ -1,50 +1,35 @@
 let passwordHash = require('password-hash');
-let { argv } = require('yargs');
-let { dbUsername, dbPassword } = argv;
-const mysql = require('mysql');
+let connection = require('../database');
 
 const endpoint = async function (request, response) {
-    console.log(request.body);
-    let { username, password, repassword:extrapass } = request.body;
-    console.log(username, password, extrapass);
+    let { username, password, repassword: extrapass } = request.body;
     if (password !== extrapass)
         return response.send(JSON.stringify({
             success: false,
             data: "Password mismatch"
         }))
-    let connection = mysql.createConnection({
-        host: "localhost",
-        user: dbUsername,
-        password: dbPassword,
-        database: "sql_database",
-    });
-    connection.connect(e => {
-        if (e) {
-            return response.send(e);
+    connection.query("SELECT id as username FROM user_credentials", (err, res, fields) => {
+        if (err) {
+            return response.send(err);
         }
-        connection.query("SELECT id as username FROM user_credentials", (err, res, fields) => {
-            if(err){
+        let username_list = res.map(e => e.username);
+        if (username_list.includes(username))
+            return response.send(JSON.stringify({
+                success: false,
+                data: "Username exists"
+            }))
+
+        let hashedPass = passwordHash.generate(password, { algorithm: 'sha256', iterations: 5, saltLength: 32 });
+        connection.query(`INSERT INTO user_credentials (id, password) VALUES ("${username}","${hashedPass}")`, (err, res, fields) => {
+            if (err) {
                 return response.send(err);
             }
-            let username_list = res.map(e => e.username);
-            if (username_list.includes(username))
-                return response.send(JSON.stringify({
-                    success: false,
-                    data: "Username exists"
-                }))
+            return response.send(JSON.stringify({
+                success: true,
+                data: null
+            }))
+        })
 
-            let hashedPass = passwordHash.generate(password, { algorithm: 'sha256', iterations: 5, saltLength: 32 });
-            connection.query(`INSERT INTO user_credentials (id, password) VALUES ("${username}","${hashedPass}")`, (err, res, fields)=>{
-                if(err){
-                    return response.send(err);
-                }
-                return response.send(JSON.stringify({
-                    success: true,
-                    data: null
-                }))
-            })
-
-        });
     });
 }
 
