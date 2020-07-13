d245240048a6881d6ff8f5b3c8e1229c1fe637d2
Gibbsface <cole.gibbs@gmail.com>
2020-07-12 23:46:06 -0500

    not sure why this file wasnt included in the branch I just merged

--- endpoints/profile_update.js
+++ endpoints/profile_update.js
@@ -3,18 +3,29 @@ let { dbUsername, dbPassword } = argv;
 let connection = require("../database");
 
 const endpoint = function(request, response){
-    connection.query(
-        `SELECT * FROM client_information WHERE id = '${request.username}'`,
-        function (error, rows, fields) {
-          if (!!error) {
-            console.log("Error in Query");
-          } else {
-            console.log("Successful Query", rows);
-            response.contentType("application/json");
-            response.json(rows);
-          }
-        }
-      );
+  connection.query(
+    `UPDATE client_information SET full_name = '${request.body.fullName}' WHERE id = '${request.username}'`,
+    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
+
+  connection.query(
+    `UPDATE client_information SET address_1 = '${request.body.addr1}' WHERE id = '${request.username}'`,
+    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
+
+  connection.query(
+    `UPDATE client_information SET address_2 = '${request.body.addr2}' WHERE id = '${request.username}'`,
+    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
+
+  connection.query(
+    `UPDATE client_information SET city = '${request.body.city}' WHERE id = '${request.username}'`,
+    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
+
+  connection.query(
+    `UPDATE client_information SET state = '${request.body.state}' WHERE id = '${request.username}'`,
+    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
+
+  connection.query(
+    `UPDATE client_information SET zipcode = ${request.body.zip} WHERE id = '${request.username}'`,
+    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
 }
 
 module.exports = endpoint;
