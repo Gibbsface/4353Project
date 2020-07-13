76325c372892de2039a073dc5de6858c134ef95c
Tristan Jones <tristankylerjones@gmail.com>
2020-07-12 23:58:51 -0500

    Condense the response.json

--- endpoints/profile_info.js
+++ endpoints/profile_info.js
@@ -1,28 +1,18 @@
-let { argv } = require("yargs");
-let { dbUsername, dbPassword } = argv;
 let connection = require("../database");
 
-const endpoint = function(request, response){
-    connection.query(
-        `SELECT * FROM client_information WHERE id = '${request.username}'`,
-        function (error, rows, fields) {
-          if (!!error) {
-            console.log("Error in Query");
-          } else {
-            console.log("Successful Query");
-            const profile = {
-              fullName: rows[0].full_name,
-              addr1: rows[0].address_1,
-              addr2: rows[0].address_2,
-              city: rows[0].city,
-              state: rows[0].state,
-              zip: rows[0].zipcode
-          };
-            response.contentType("application/json");
-            response.json(profile);
-          }
-        }
-      );
-}
+const endpoint = function (request, response) {
+  connection.query(
+    `SELECT * FROM client_information WHERE id = '${request.username}'`,
+    function (error, rows, fields) {
+      if (!!error) {
+        //console.log("Error in Query");
+      } else {
+        //console.log("Successful Query");
+        response.contentType("application/json");
+        response.json(rows);
+      }
+    }
+  );
+};
 
 module.exports = endpoint;
