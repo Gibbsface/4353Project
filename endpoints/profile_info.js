2af4ca93fb5d895cf856c7d498b9370ba8df8c93
Gibbsface <cole.gibbs@gmail.com>
2020-07-13 00:42:29 -0500

    will create a new row for client info if it does not exist, instead of throwing errors

--- endpoints/profile_info.js
+++ endpoints/profile_info.js
@@ -10,14 +10,14 @@ const endpoint = function(request, response){
             console.log("Error in Query");
           } else {
             console.log("Successful Query");
-            const profile = {
-              fullName: rows[0].full_name,
-              addr1: rows[0].address_1,
-              addr2: rows[0].address_2,
-              city: rows[0].city,
-              state: rows[0].state,
-              zip: rows[0].zipcode
-          };
+            const profile = (rows.length > 0) ? {
+                fullName: rows[0].full_name,
+                addr1: rows[0].address_1,
+                addr2: rows[0].address_2,
+                city: rows[0].city,
+                state: rows[0].state,
+                zip: rows[0].zipcode
+              } : {};
             response.contentType("application/json");
             response.json(profile);
           }
--- endpoints/profile_update.js
+++ endpoints/profile_update.js
@@ -3,29 +3,12 @@ let { dbUsername, dbPassword } = argv;
 let connection = require("../database");
 
 const endpoint = function(request, response){
-  connection.query(
-    `UPDATE client_information SET full_name = '${request.body.fullName}' WHERE id = '${request.username}'`,
-    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
 
   connection.query(
-    `UPDATE client_information SET address_1 = '${request.body.addr1}' WHERE id = '${request.username}'`,
-    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
-
-  connection.query(
-    `UPDATE client_information SET address_2 = '${request.body.addr2}' WHERE id = '${request.username}'`,
-    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
-
-  connection.query(
-    `UPDATE client_information SET city = '${request.body.city}' WHERE id = '${request.username}'`,
-    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
-
-  connection.query(
-    `UPDATE client_information SET state = '${request.body.state}' WHERE id = '${request.username}'`,
-    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
-
-  connection.query(
-    `UPDATE client_information SET zipcode = ${request.body.zip} WHERE id = '${request.username}'`,
-    function (error, rows, fields) { !!error ? console.log("Error in Query") : console.log("Successful Query")});
+    `REPLACE INTO client_information (id, full_name, address_1, address_2, city, state, zipcode)
+    VALUES ('${request.username}', '${request.body.fullName}', '${request.body.addr1}', '${request.body.addr2}', 
+    '${request.body.city}', '${request.body.state}', ${request.body.zip})`,
+    (error, n)=>{ !!error ? console.log("Query Error", error) : console.log("Successful Update(s)") });
 }
 
 module.exports = endpoint;
