6077ffce3f941663f261d40de5b288745791aafb
Tristan Jones <tristankylerjones@gmail.com>
2020-07-12 23:53:37 -0500

    Profile now update database

--- endpoints/profile_update.js
+++ endpoints/profile_update.js
@@ -1,20 +1,45 @@
-let { argv } = require("yargs");
-let { dbUsername, dbPassword } = argv;
 let connection = require("../database");
 
-const endpoint = function(request, response){
-    connection.query(
-        `SELECT * FROM client_information WHERE id = '${request.username}'`,
-        function (error, rows, fields) {
-          if (!!error) {
-            console.log("Error in Query");
-          } else {
-            console.log("Successful Query", rows);
-            response.contentType("application/json");
-            response.json(rows);
-          }
-        }
+const endpoint = function (request, response) {
+  let { full_name, address_1, address_2, city, state, zipcode } = request.body;
+
+  if (full_name == undefined) {
+    [{ full_name }] = request.body.profile;
+  }
+  if (address_1 == undefined) {
+    [{ address_1 }] = request.body.profile;
+  }
+  if (address_2 == undefined) {
+    [{ address_2 }] = request.body.profile;
+  }
+  if (city == undefined) {
+    [{ city }] = request.body.profile;
+  }
+  if (state == undefined) {
+    [{ state }] = request.body.profile;
+  }
+  if (zipcode == undefined) {
+    [{ zipcode }] = request.body.profile;
+  }
+
+  connection.query(
+    `UPDATE client_information 
+    SET full_name = "${full_name}", address_1 = "${address_1}", address_2 = "${address_2}", city = "${city}", state = "${state}", zipcode= "${zipcode}"
+    WHERE id = "${request.username}"`,
+    (err, res, fields) => {
+      if (err) {
+        console.log("Error: Cannot Post");
+        return response.send(err);
+      }
+      console.log("Post Successful");
+      return response.send(
+        JSON.stringify({
+          success: true,
+          data: null,
+        })
       );
-}
+    }
+  );
+};
 
 module.exports = endpoint;
