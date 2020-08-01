fbd4f6971d73caff4c8b99a1f2ffb160db218a7f
Tristan Jones <tristankylerjones@gmail.com>
2020-08-01 14:38:14 -0500

    Update fuel_quote_post.js

--- endpoints/fuel_quote_post.js
+++ endpoints/fuel_quote_post.js
@@ -1,23 +1,21 @@
 let connection = require("../database");
 
-//NOTE for TAs, this is our pricing module.
 const endpoint = async function (request, response) {
-  let { gallons_requested, delivery_date } = request.body;
+  let { gallons_requested, delivery_date, margin, total } = request.body;
   //Sending address to frontend
   connection.query(
     `SELECT address_1 FROM client_information WHERE id = '${request.username}'`,
     function (error, rows, fields) {
       let address = rows[0].address_1;
-
       connection.query(
         `INSERT INTO fuel_quote VALUES ("${request.username}","","${gallons_requested}",
-        "${address}","${delivery_date}","0","0")`,
+        "${address}","${delivery_date}","${margin}","${total}")`,
         (err, res, fields) => {
           if (err) {
-            console.log("Error: Cannot Post");
+            //console.log("Error: Cannot Post");
             return response.send(err);
           }
-          console.log("Post Successful");
+          //console.log("Post Successful");
           return response.send(
             JSON.stringify({
               success: true,
