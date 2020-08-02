c1417155d264d686a6ac346f3543faabb7f1c6ab
Gibbsface <cole.gibbs@gmail.com>
2020-08-02 02:21:22 -0500

    warnings have been conquered

--- project-frontend/src/components/Login.js
+++ project-frontend/src/components/Login.js
@@ -1,31 +1,22 @@
 import React, { Component } from "react";
 import "react-bulma-components/dist/react-bulma-components.min.css";
 
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
 function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') 
+    {
+      c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) 
+        return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return null;
 }
 
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
-export class Login extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -58,7 +49,7 @@ export class Login extends Component {
       password: this.state.password
     }))
     xhttp.onreadystatechange = (e) => {
-      if (xhttp.readyState == XMLHttpRequest.DONE) {
+      if (xhttp.readyState === XMLHttpRequest.DONE) {
         let body = JSON.parse(xhttp.response);
         if(body.success){
           this.props.login();
